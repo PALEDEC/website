4f010a708a4fc1aaca69a299d101440b92188c6a
woleekanola <woleekanola@gmail>
2022-05-07 21:55:00 +0100

    update on netlify cms

--- frntnd/pages/index.js
+++ frntnd/pages/index.js
@@ -4,11 +4,29 @@ import MainBanner from '../components/ItAgency/MainBanner';
 import OurWorks from '../components/DefaultHome/OurWorks';
 import CtaAreaTwo from '../components/Common/CtaAreaTwo';
 import Footer from '../components/Layouts/Footer';
+import { useEffect, Fragment } from 'react'
+import Head from 'next/head'
 
 const Home =()=>{
 
+    useEffect(() => {
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on('init', (user) => {
+            if (!user) {
+              window.netlifyIdentity.on('login', () => {
+                document.location.href = '/admin/'
+              })
+            }
+          })
+        }
+      }, [])
+
     return(
     <>
+      <Head>
+        {/* <title>Netlify CMS && Next.js Starter</title> */}
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
          <NavbarTwo />
                 
                 <MainBanner /> 
